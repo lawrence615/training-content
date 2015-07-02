d91fca56b5e2bd6138107294851a5bd5121fc7b5
Lawrence Macharia <macharialawrence2011@gmail.com>
2015-07-02 08:09:58 +0300

    added more functionality

--- code/MPesa/app/src/main/java/com/mobidev/mpesa/PhoneNoActivity.java
+++ code/MPesa/app/src/main/java/com/mobidev/mpesa/PhoneNoActivity.java
@@ -1,13 +1,16 @@
 package com.mobidev.mpesa;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class PhoneNoActivity extends AppCompatActivity {
@@ -25,30 +28,18 @@ public class PhoneNoActivity extends AppCompatActivity {
         btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String phone_no = edPhoneNo.getText().toString();
+                if (TextUtils.isEmpty(phone_no)) {
+                    Toast.makeText(getApplicationContext(), "Please provide phone number", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                
 
+                Intent intent = new Intent(getApplicationContext(), AmountActivity.class);
+                intent.putExtra(AmountActivity.ARG_PHONE_NO, phone_no);
+                startActivity(intent);
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_phone_no, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
