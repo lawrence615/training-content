4696700ff10a2beb51a49655a02f4b75c79ca35a
Lawrence Macharia <macharialawrence2011@gmail.com>
2015-07-24 10:35:00 +0300

    initial commit

--- code/WidgetExample/app/src/main/java/com/mobidev/widgetexample/SaveMeReceiver.java
+++ code/WidgetExample/app/src/main/java/com/mobidev/widgetexample/SaveMeReceiver.java
@@ -1,7 +1,29 @@
 package com.mobidev.widgetexample;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
 /**
  * Created by Lawrence on 7/24/15.
  */
-public class SaveMeReceiver {
+public class SaveMeReceiver extends BroadcastReceiver {
+
+    public SaveMeReceiver() {
+
+    }
+
+
+    /**
+     * whenever an event occurs Android calls onReceive() method on all registered broadcast receivers
+     *
+     * @param context
+     * @param intent
+     */
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Toast.makeText(context, "Received. React now!", Toast.LENGTH_SHORT).show();
+    }
 }
