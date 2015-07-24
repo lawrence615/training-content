0fc105cc7a1971860a5dc6d583f3c49e96706a5b
Lawrence Macharia <macharialawrence2011@gmail.com>
2015-07-24 10:34:24 +0300

    we are now able to send a message when a user clicks on the widget

--- code/WidgetExample/app/src/main/java/com/mobidev/widgetexample/AppWidgetProvider.java
+++ code/WidgetExample/app/src/main/java/com/mobidev/widgetexample/AppWidgetProvider.java
@@ -2,9 +2,16 @@ package com.mobidev.widgetexample;
 
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import android.telephony.SmsManager;
+import android.view.View;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -13,25 +20,60 @@ import java.util.Random;
  */
 public class AppWidgetProvider extends android.appwidget.AppWidgetProvider {
 
+
+    private static final String SAVE_ME_CLICKED = "widget_save_button_clicked";
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        final int count = appWidgetIds.length;
+        // Get all ids
+        ComponentName thisWidget = new ComponentName(context, AppWidgetProvider.class);
+        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
 
-        for (int i = 0; i < count; i++) {
-            int widgetId = appWidgetIds[i];
-            String number = String.format("%03d", (new Random().nextInt(900) + 100));
+        for (int widgetId : allWidgetIds) {
 
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-                    R.layout.activity_main);
-//            remoteViews.setTextViewText(R.id.textView, number);
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.activity_main);
 
-            Intent intent = new Intent(context, AppWidgetProvider.class);
-            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
-                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.imageView, pendingIntent);
-//            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+            remoteViews.setOnClickPendingIntent(R.id.imageView, getPendingSelfIntent(context, SAVE_ME_CLICKED));
+
+
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
     }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);//add this line
+
+        if (SAVE_ME_CLICKED.equals(intent.getAction())) {
+//            Intent newIntent = new Intent(this, SaveMeReceiver.class);
+//            intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+//            intent.setAction("com.mobidev.widgetexample.BroadcastReceiver");
+//            intent.putExtra("command", "SendSMS");
+//            sendBroadcast(newIntent);
+
+            sendSMS(context);
+
+        }
+    }
+
+
+    public void sendSMS(Context context) {
+
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+
+        int number = sp.getInt(PrefsConstants.PHONE_NO_KEY, 0);
+        String message = sp.getString(PrefsConstants.MESSAGE_KEY, null);
+        if (number != 0 && message != null) {
+            SmsManager.getDefault().sendTextMessage("+254" + number, null, message, null, null);
+        }
+    }
+
+
+    protected PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, getClass());
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
+    }
+
+
 }
